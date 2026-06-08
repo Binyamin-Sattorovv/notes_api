8da63b0bafd40edde5bf5d4a04d825da21b3195a
Binyamin-Sattorov <binyaminnn.2000@gmail.com>
2026-06-08 22:30:31 +0500

    add crud app

---
+++ app/crud.py
@@ -0,0 +1,56 @@
+from sqlalchemy.orm import Session
+
+from models import Note
+
+
+def create_note(db: Session, title: str, content: str):
+    
+    note = Note(title=title, content=content)
+    
+    db.add(note)
+    
+    db.commit()
+    
+    db.refresh(note)
+    
+    return note
+
+
+def read_note(db: Session):
+    
+    return db.query(Note).all()
+
+
+def update_note(db: Session, note_id: int, title: str, content: str):
+    
+    note = db.query(Note).filter(Note.id == note_id).first()
+    
+    if not note:
+        
+        return None
+    
+    note.title = title
+    
+    note.content = content
+    
+    db.refresh(note)
+    
+    db.commit()
+    
+    return note
+
+
+def delete_note(db: Session, note_id: int):
+    
+    note = db.query(Note).filter(Note.id == note_id).first()
+    
+    if not note:
+        
+        return None
+    
+    db.delete(note)
+    
+    db.commit()
+    
+    return True
+
